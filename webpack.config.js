c0294e741d8874fe2d50e64ee01a786e2edfb9c0
Euibin Cheon <euibinc@gmail.com>
2016-11-21 01:33:21 -0500

    Change project name

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: "./app/crime_data.jsx",
+  entry: "./app/crime_graph.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
